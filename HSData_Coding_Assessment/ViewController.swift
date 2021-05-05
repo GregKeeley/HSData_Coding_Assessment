cc4e9c5769a5bc2c3dbe5670c6d0ff29e823aaf4
Gregory Keeley <gregoryKeeley@pursuit.org>
2021-05-05 11:53:19 -0400

    Initial Commit

---
+++ HSData_Coding_Assessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HSData_Coding_Assessment
+//
+//  Created by Gregory Keeley on 5/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HSData_Coding_AssessmentTests/HSData_Coding_AssessmentTests.swift
@@ -0,0 +1,33 @@
+//
+//  HSData_Coding_AssessmentTests.swift
+//  HSData_Coding_AssessmentTests
+//
+//  Created by Gregory Keeley on 5/5/21.
+//
+
+import XCTest
+@testable import HSData_Coding_Assessment
+
+class HSData_Coding_AssessmentTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
